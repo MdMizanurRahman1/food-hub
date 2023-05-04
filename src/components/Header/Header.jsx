84dce8c456522eb200deff54888ea8e1e3039223
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-05-04 12:40:35 +0300

    active route added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { Button, Container, Image, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../Providers/AuthProvider';
 
 const Header = () => {
     const { user, logOut } = useContext(AuthContext);
+    const location = useLocation();
 
     const handleLogOut = () => {
         logOut()
@@ -15,23 +16,27 @@ const Header = () => {
             });
     }
 
+    const getNavLinkClass = (path) => {
+        return location.pathname === path ? 'text-primary' : 'text-white';
+    }
+
     return (
         <div className='banner'>
             <Navbar>
                 <Container>
-                    <Navbar.Brand href='#' className='text-primary fs-2'>Thai Food Hubs</Navbar.Brand>
+                    <Navbar.Brand href='#' className='text-white fs-2'>Thai Food Hubs</Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto gap-4'>
-                            <Link to='/' className='text-decoration-none '>
+                            <Link to='/' className={`text-decoration-none ${getNavLinkClass('/')}`}>
                                 Home
                             </Link>
-                            <Link to='/blog' className='text-decoration-none '>
+                            <Link to='/blog' className={`text-decoration-none ${getNavLinkClass('/blog')}`}>
                                 Blog
                             </Link>
                         </Nav>
 
-                        <Nav className='align-items-center justify-content-center ms-5'>
+                        <Nav className='align-items-center justify-content-center text-white ms-5'>
                             {user && <Image src='/profile.jpg' roundedCircle className='mx-2' style={{ width: '30px', height: '30px' }} />}
 
                             {user ?
